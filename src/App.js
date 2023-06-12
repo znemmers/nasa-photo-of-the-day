b0b6f011aacdffc19c7e338f45dbe1d3196cbf24
Zach Nemmers <Zach@Zachs-MacBook-Pro-3.local>
2023-06-12 11:23:32 -0500

    completed project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
+import NasaPhoto from './Components/NasaPhoto'
 
 function App() {
+  const [data, setData] = useState()
+  const apiKey = process.env.API_KEY
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=zhDE369sBVmFq2jcXVTWw3F0NEcLqdLce1S6Kays')
+      .then(res => {
+        console.log("hello", res)
+        setData(res.data)
+      })
+      .catch(err => console.error(err))
+  }, []);
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+     {data && <NasaPhoto photo={data} />}
     </div>
   );
 }
